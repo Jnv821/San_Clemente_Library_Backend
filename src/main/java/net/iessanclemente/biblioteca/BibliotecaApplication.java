43b62022c955a41946241e4c381c49de33783793
jnv821 <jnv821@gmail.com>
2024-03-29 18:17:58 +0100

    Added rest controller annotation.

--- src/main/java/net/iessanclemente/biblioteca/BibliotecaApplication.java
+++ src/main/java/net/iessanclemente/biblioteca/BibliotecaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@RestController
 public class BibliotecaApplication {
 
 	public static void main(String[] args) {
